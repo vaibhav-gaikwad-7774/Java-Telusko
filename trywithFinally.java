7ec2507913c05111898c9ec23f0693f01ce436d9
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-20 22:34:11 +0530

    final 20 Dec

---
+++ trywithFinally.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class trywithFinally {
+    public static void main(String[] args) {
+
+        int num = 0;
+
+        try {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the Number which is you want to print: ");
+            num = sc.nextInt();
+
+            System.out.println(num);
+
+        } finally
+
+        {
+            // sc.close();
+
+        }
+
+    }
+}
