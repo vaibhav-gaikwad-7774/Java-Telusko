2cbfba5264c819505fad679f484e3c9e6d92a115
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-22 22:25:50 +0530

    dec 22

--- CollectionDemo.java
+++ CollectionDemo.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class CollectionDemo {
     public static void main(String[] args) {
-        Set<Integer> nums = new HashSet<Integer>();
+        Collection<Integer> nums = new TreeSet<Integer>();
         nums.add(6);
         nums.add(5);
         nums.add(7);
@@ -13,12 +16,18 @@ public class CollectionDemo {
         nums.add(1);
         nums.add(6);
 
+        Iterator<Integer> Values = nums.iterator();
+
+        while (Values.hasNext()) {
+            System.out.println(Values.next());
+        }
+
         // System.out.println(nums.get(2));
 
-        for (int n : nums) {
-            // int num = (Integer) n;
-            System.out.println(n);
-        }
+        // for (int n : nums) {
+        // // int num = (Integer) n;
+        // System.out.println(n);
+        // }
 
         // System.out.println(nums);
 
