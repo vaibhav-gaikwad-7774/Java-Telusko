d5257056e9e04db110a84a02dc505c8de881090b
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-22 12:13:24 +0530

    dec 22

--- Threads.java
+++ Threads.java
@@ -2,41 +2,61 @@ class z {
 
 }
 
-class A implements Runnable {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hi");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
-}
+// class A implements Runnable {
+// public void run() {
+// for (int i = 1; i <= 5; i++) {
+// System.out.println("Hi");
+// try {
+// Thread.sleep(10);
+// } catch (InterruptedException e) {
+// e.printStackTrace();
+// }
+// }
+// }
+// }
 
-class B implements Runnable {
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("hello");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
-}
+// class B implements Runnable {
+// public void run() {
+// for (int i = 1; i <= 5; i++) {
+// System.out.println("hello");
+// try {
+// Thread.sleep(10);
+// } catch (InterruptedException e) {
+// e.printStackTrace();
+// }
+// }
+// }
+// }
 
 public class Threads {
     public static void main(String[] args) {
-        A obj1 = new A();
-        B obj2 = new B();
+        Runnable obj1 = () -> { // applying lambda function which is used
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
-        obj1.start();
-        obj2.start();
+        Runnable obj2 = () -> { // applying lambda function which is used
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
 
     }
 
