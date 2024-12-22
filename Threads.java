6a45f87fc8fcda22052c3c1c865281cc2225ddbf
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-22 11:55:56 +0530

    dec 22

--- Threads.java
+++ Threads.java
@@ -1,15 +1,31 @@
-class A extends Thread {
+class z {
+
+}
+
+class A implements Runnable {
     public void run() {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
 
-class B extends Thread {
+class B implements Runnable {
     public void run() {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -18,10 +34,6 @@ public class Threads {
     public static void main(String[] args) {
         A obj1 = new A();
         B obj2 = new B();
-
-        System.out.println(obj1.getPriority());
-
-        obj2.setPriority(Thread.MAX_PRIORITY);
 
         obj1.start();
         obj2.start();
