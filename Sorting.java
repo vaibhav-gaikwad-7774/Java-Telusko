3fdae18089458a5f6ed5e983f1f666bf0b134910
Vaibhav Gaikwad <GFG19667@gfg.geeksforgeeks.org>
2024-12-23 10:13:54 +0530

    23 Dec

---
+++ Sorting.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Sorting {
+    public static void main(String[] args) {
+        List<Integer> nums =  new ArrayList<>();
+        nums.add(3);
+        nums.add(5);
+        nums.add(8);
+        nums.add(1);
+        nums.add(7);
+
+        Collections.sort(nums);
+        System.out.println(nums);
+
+
+        
+    }
+    
+}
