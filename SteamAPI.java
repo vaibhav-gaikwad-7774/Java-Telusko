bc733975865cafb872d085fc249545213ea4e8d3
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-23 21:57:21 +0530

    23 dec

---
+++ SteamAPI.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class SteamAPI {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(2, 3, 4, 5);
+
+        // Consumer<Integer> con = n -> System.out.println(n);
+
+        nums.forEach(n -> System.out.println(n));
+
+        // for (int i = 0; i < nums.size(); i++) {
+        // System.out.println(i);
+        // }
+
+        // for (int n : nums) {
+        // System.out.println(n);
+        // }
+
+        // nums.forEach(n -> System.out.println(n));
+
+        // int sum = 0;
+        // for (int n : nums) {
+
+        // if (n % 2 == 0) {
+        // n = n * 2;
+        // sum = sum + n;
+        // }
+        // }
+
+        // System.out.println(sum);
+    }
+
+}
