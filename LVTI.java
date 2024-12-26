c9b27f6f9c7a81c7d6ec89ff5aeb484c21ef325d
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-26 22:26:48 +0530

    26 dec

--- LVTI.java
+++ LVTI.java
@@ -1,23 +1,31 @@
 public class LVTI {
-    // var num = 10;
-    // var keywrd cant used as a class name
+    // var num = 10; // Not allowed: `var` cannot be used as a class-level field
 
     public static void main(String[] args) {
-        int a = 9;
-        var b = 8;
-        // System.out.println(num);
-        String var = "Vaibhav";
+        int a = 9; // Regular declaration
+        var b = 8; // `var` correctly infers the type as int
+        // System.out.println(num); // `num` is not defined
+
+        String varName = "Vaibhav"; // Using "var" as a variable name is valid
 
         int c;
-        // var d; // when we intialize the any variable with the
-        // var type we need to definitely assign the value of the
-        // integer in the following array type
+        // var d; // Error: `var` requires initialization at the time of declaration
 
-        int nums[] = new int[10];
+        int[] nums = new int[10]; // Regular array declaration
 
-        var nums1 = new int[10]; // is allowed
+        var nums1 = new int[10]; // `var` infers the type as int[]
 
-        var obj = new Alien();
+        var obj = new Alien(); // Correct instantiation of an object using `var`
+
+        System.out.println("Value of b: " + b);
+        System.out.println("Value of varName: " + varName);
     }
+}
 
+// Example class for object creation
+class Alien {
+    @Override
+    public String toString() {
+        return "Alien object created!";
+    }
 }
