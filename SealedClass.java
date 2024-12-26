842b28a157c2fa323afa3930db74b3171f74108e
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-26 21:39:11 +0530

    26 dec

--- SealedClass.java
+++ SealedClass.java
@@ -1,22 +1,36 @@
-sealed class A permits B, C {
-
+sealed class A extends Thread implements Cloneable permits B, C {
+    // Class A logic
 }
 
 non-sealed class B extends A {
-
+    // Class B logic
 }
 
 final class C extends A {
-
+    // Class C logic
 }
 
+// Class D extends non-sealed class B, so further subclassing is allowed
 class D extends B {
+    // Class D logic
+}
 
+// Sealed interface example
+sealed interface X permits Y {
+    // Interface X logic
+}
+
+final class Y implements X {
+    // Class Y logic
 }
 
 public class SealedClass {
     public static void main(String[] args) {
+        // Example usage of the classes
+        A a = new B();
+        A c = new C();
+        B b = new D();
 
+        System.out.println("Sealed class example executed.");
     }
-
 }
