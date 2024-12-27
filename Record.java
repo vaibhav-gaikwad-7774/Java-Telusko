5811585b03caae70d16647d965fa5b529cb2db9f
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-27 21:36:46 +0530

    27 dec

--- Record.java
+++ Record.java
@@ -1,4 +1,22 @@
-record Alien(int id, String name) {
+record Alien(int id, String name) implements Cloneable {
+    static int num;
+    // int age; not declared in the followiing way
+
+    public Alien {
+        if (id == 0) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public void show() {
+
+    }
+
+    // public Alien(int id, String name) {
+    // this.id = id;
+    // this.name = name;
+    // }
 
 }
 
@@ -8,7 +26,7 @@ public class Record {
         Alien a2 = new Alien(1, "navin");
 
         System.out.println(a1.equals(a2));
-        System.out.println(a1);
+        System.out.println(a1.name());
 
     }
 
