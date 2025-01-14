f7c2277fac3ab56123cdc2709a30ec6c9a1c1892
Vaibhav Gaikwad <GFG19667@gfg.geeksforgeeks.org>
2025-01-14 18:05:08 +0530

    The working steps known by the programm of linear and binary search

--- AdvanceJava/DSA/BinarySearchDemo.java
+++ AdvanceJava/DSA/BinarySearchDemo.java
@@ -22,21 +22,29 @@ public class BinarySearchDemo {
     }
 
     public static int linearSearch(int[] nums, int target) {
+        int steps = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target)
+            steps++;
+            if (nums[i] == target) {
+                System.out.println("Steps Taken by Linear: " + steps);
                 return i;
+            }
         }
+        System.out.println("Steps Taken by Linear: " + steps);
         return -1;
     }
 
     public static int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        int steps = 0;
 
         while (left <= right) {
+            steps++;
             int mid = (left + right) / 2;
 
             if (nums[mid] == target) {
+                System.out.println("Steps Taken by binary: " + steps);
                 return mid;
             } else if (nums[mid] < target) {
                 left = mid + 1;
@@ -44,6 +52,7 @@ public class BinarySearchDemo {
                 right = mid - 1;
             }
         }
+        System.out.println("Steps Taken by Linear: " + steps);
         return -1;
     }
 }
