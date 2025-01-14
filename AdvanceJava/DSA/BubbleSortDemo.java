7caa8f609125959724d47730f7f497420ba75622
Vaibhav Gaikwad <GFG19667@gfg.geeksforgeeks.org>
2025-01-14 18:28:01 +0530

    14 jan

---
+++ AdvanceJava/DSA/BubbleSortDemo.java
@@ -0,0 +1,21 @@
+package DSA;
+
+public class BubbleSortDemo {
+    public static void main(String[] args) {
+        int nums[] = {1,5,7,8,4,2};
+
+
+    System.out.println("Before Sorting");
+    for(int num : nums){
+        System.out.println(num + " ");
+    }
+    
+
+    
+    System.out.println("Before Sorting");
+    for(int num : nums){
+        System.out.println(num + " ");
+    }
+    
+}
+}
