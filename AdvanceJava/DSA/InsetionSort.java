1d12b2d0596871597b76c0e5e94cd1e68ef2ea64
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2025-01-15 21:52:23 +0530

    final 15 jan

--- AdvanceJava/DSA/InsetionSort.java
+++ AdvanceJava/DSA/InsetionSort.java
@@ -1,5 +1,23 @@
 package DSA;
 
 public class InsetionSort {
-    
+    public static void main(String[] args) {
+        int[] nums = { 2, 3, 1, 5, 8 };
+
+        System.out.println("Before Sorting: ");
+        for (int num : nums) {
+            System.out.println(num + " ");
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+
+            while (j >= 0) {
+
+            }
+        }
+
+    }
+
 }
